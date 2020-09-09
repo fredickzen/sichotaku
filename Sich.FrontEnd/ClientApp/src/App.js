a6a2ed80dfe64fa852165d6cbfbb652254decfd8
framosmultisf <framos@multisf.cl>
2020-09-08 23:54:36 -0300

    front antdesing cambio layouts

--- Sich.FrontEnd/ClientApp/src/App.js
+++ Sich.FrontEnd/ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './layouts/Layout.jsx';
 
+import 'antd/dist/antd.css';
 import './assets/css/custom.css'
 import Home from './components/pages/Home/Home';
 
