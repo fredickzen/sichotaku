ed73ea9529abf25e43bff2c9e53280d03bc5cc8e
framosmultisf <framos@multisf.cl>
2020-09-09 01:05:39 -0300

    front brian ql cagado de miedo

--- Sich.FrontEnd/ClientApp/src/App.js
+++ Sich.FrontEnd/ClientApp/src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './layouts/Layout.jsx';
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./layouts/Layout.jsx";
 
-import 'antd/dist/antd.css';
-import './assets/css/custom.css'
-import Home from './components/pages/Home/Home';
+import "antd/dist/antd.css";
+import "./assets/css/custom.css";
+import Home from "./components/pages/Home/Home";
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
+        <Route exact path="/" component={Home} />
       </Layout>
     );
   }
